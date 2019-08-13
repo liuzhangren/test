b2ffc5ec90215f8cfb42f4afc9be9fdd36d4202f
liuzhangren100 <759551480@qq.com>
2019-08-13 18:50:30 +0800

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+const data = [
+  {province: 'hubei', city: 'wuhan', county: 'aaa'},
+  {province: 'hubei', city: 'wuhan', county: 'aaa111'},
+  {province: 'hubei', city: 'yichang', county: 'bbb'},
+  {province: 'hubei', city: 'xiangyang', county: 'ccc'},
+  {province: 'hunan', city: 'changsha', county: 'aaaa'},
+]
